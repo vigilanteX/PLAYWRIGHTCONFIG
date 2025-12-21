10865b71722ebafd97945edb3e27c825708d3878
vigilanteX <dixitaman8@gmail.com>
2025-12-21 23:04:05 +0530

    junit reporting

--- myconfig.js
+++ myconfig.js
@@ -11,7 +11,7 @@ const myCustomConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["json", { outputFile: "results.json" }]],
+  reporter: [["junit", { outputFile: "resultsxml.xml" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
