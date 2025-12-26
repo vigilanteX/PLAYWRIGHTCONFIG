83b43a79333b36367fb5121666a7eb3ea65772c6
vigilanteX <dixitaman8@gmail.com>
2025-12-26 20:15:18 +0530

    forbidOnly

--- gptmyconfig.js
+++ gptmyconfig.js
@@ -1,7 +1,8 @@
 import { devices } from "@playwright/test";
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 export const config = {
-  testDir: "./redoconfigsGPT/05globalTimeout",
+  testDir: "./redoconfigsGPT/06forbidOnly",
   timeout: 25000,
   globalTimeout: 40000,
+  forbidOnly: true,
 };
---
+++ redoconfigsGPT/06forbidOnly/demo.spec.js
@@ -0,0 +1,3 @@
+import { test } from "@playwright/test";
+test("t1", function () {});
+test.only("t2", function () {});
