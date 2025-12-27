65f8e01c43b65b3cdb00a375113784b7d47a3c3c
vigilanteX <dixitaman8@gmail.com>
2025-12-27 17:37:22 +0530

    viewport

--- gptmyconfig.js
+++ gptmyconfig.js
@@ -1,9 +1,13 @@
 import { devices } from "@playwright/test";
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 export const config = {
-  testDir: "./redoconfigsGPT/UseObject/02baseURL",
+  testDir: "./redoconfigsGPT/UseObject/03viewport",
   use: {
     headless: false,
     baseURL: "https://naveenautomationlabs.com",
+    viewport: {
+      height: 300,
+      width: 280,
+    },
   },
 };
