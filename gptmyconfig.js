cf85bacccaa6a928d79d0927751ca1c0e1174d89
vigilanteX <dixitaman8@gmail.com>
2025-12-27 17:23:14 +0530

    use-headless

--- gptmyconfig.js
+++ gptmyconfig.js
@@ -1,14 +1,8 @@
 import { devices } from "@playwright/test";
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 export const config = {
-  testDir: "./redoconfigsGPT/16webserver",
+  testDir: "./redoconfigsGPT/UseObject/01headless",
   use: {
     headless: false,
-  },
-  webServer: {
-    url: "http://localhost:9000",
-    timeout: 10000,
-    command: "node ./myserver.js",
-    stdout: "pipe",
   },
 };
